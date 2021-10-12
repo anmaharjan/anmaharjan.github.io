af19da8a7672b93b041744a1079d04fb533ebf15
Anish Maharjan <amaharjan@miu.edu>
2021-10-12 15:46:30 -0500

    clean circle, animation part left

---
+++ w3d2-clean-screen/js/circle.js
@@ -0,0 +1,70 @@
+$(function () {
+    const minPosition = 10, maxPosition = 90;
+    let circleContainer = $("#circle_container");
+    /* -- input fields -- */
+    let width = 0,growthAmount = 0, growthRate = 0, numberOfCircle = 0;
+    let startBtn = $(".input-gadgets #start");
+
+    startBtn.click(function(){
+        width =  $(".input-gadgets #width").val();
+        growthAmount = $(".input-gadgets #growth_amount").val();
+        growthRate = $(".input-gadgets #growth_rate").val()
+        numberOfCircle = $(".input-gadgets #number_of_circle").val();
+
+        emptyCircleContainer();
+        drawCircles();
+        animateCircles();
+    });
+
+    function drawCircles(){
+        for(let i=0; i<numberOfCircle; i++){
+            let top = Math.floor(Math.random() * (maxPosition - minPosition) + minPosition) + "%",
+                left = Math.floor(Math.random() * (maxPosition - minPosition) + minPosition) + "%",
+                color = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+            circleContainer.append($("<div>", {
+                "id" : "circle-"+i,
+                "class": "circle",
+                "css": {
+                    "top": top, "left": left, "background-color": color,
+                    "width": width + "px", "height": width + "px", "border-radius": width + "px",
+                },
+                "click": function (){
+                    $(this).hide();
+                }
+            }));
+        }
+    }
+
+    function animateCircles(){
+        console.log($('.circle').style());
+        // let interval = setInterval(() =>{
+        //     $('.circle')
+        //         .css({
+        //             "height": (idx, old) => parseInt(old) + growthAmount + "px",
+        //             "width": (idx, old) => parseInt(old) + growthAmount + "px",
+        //             "border-radius": (idx, old) => parseInt(old) + growthAmount + "px"
+        //         });
+        // }, growthRate);
+    }
+
+    function emptyCircleContainer(){
+        circleContainer.empty();
+    }
+
+    /*let interval = setInterval(() =>{
+        let circle = $(".circle");
+        if(parseInt(circle.css('height')) > 100){
+            clearInterval(interval);
+        }
+        $('.circle')
+            .css({
+                "height": (idx, old) => parseInt(old) + 10 + "px",
+                "width": (idx, old) => parseInt(old) + 10 + "px",
+                "border-radius": (idx, old) => parseInt(old) + 10 + "px"
+            })
+            .click(function(){
+                $(this).hide();
+            });
+    }, 250);*/
+});
